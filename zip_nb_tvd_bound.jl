0d81ab7126c7c47e77fa537ada3065c747d5abfd
JKerlidis <jamesk@ozemail.com.au>
2022-10-16 19:26:06 +1100

    Change surface plot backend to pyplot

--- zip_nb_tvd_bound.jl
+++ zip_nb_tvd_bound.jl
@@ -3,14 +3,16 @@ module CalculateTVDBounds
 include("src/BPSim.jl")
 
 using .BPSim
-using Plots
+using Plots; pyplot()
 using Plots.PlotMeasures
 
+# PyPlot.matplotlib[:rc]("font", family="serif")
+
 k = 1:50
-z_exp = 5:12
+z_exp = 4:10
 
 function zip_nb_tvd_bound(z_exp::Integer, k::Integer)
-    min(k_step_tvd_bound(k=k, z=10^z_exp, h=1, R=44.75, η=0.063, σ²=2, m=1, α=0.999), 1)
+    min(k_step_tvd_bound(k=k, z=10^z_exp, h=1, R=10.5, η=0.063, σ²=2, m=1, α=0.999), 1)
 end
 
 p = plot(
@@ -18,19 +20,22 @@ p = plot(
     k,
     zip_nb_tvd_bound,
     st = :surface,
-    title = "TVD upper bound",
-    xlabel = "\nPopulation Size",
-    ylabel = "\nTrajectory Length",
-    fontfamily = "Computer Modern",
+    title = "TVD upper bound between DCBP and PSDBP with matching moments",
+    xlabel = "\n\nPopulation Size",
+    ylabel = "\n\nTrajectory Length",
+    zlabel = "\nTVD",
+    fontfamily = "serif",
     xformatter = z_exp -> "10e"*string(trunc(Int, z_exp)),
-    camera = (40,50),
+    xguidefontrotation = -19,
+    yguidefontrotation = 47,
+    camera = (30,30),
     titlefontsize = 24,
     tickfontsize = 16,
     guidefontsize = 16,
     size = (1800, 1200),
-    top_margin = 6mm
+    legend = false
 )
 
-display(p)
+savefig(p, "/mnt/c/Users/james/OneDrive/University/Data Science Subjects/2022/Branching Processes/PSDBPs and CBPs/images/surface_plot.png")
 
 end
